2dad717b891ef2d29cf81b8f53a3ac373f8d5055
Grzegorz Uriasz <gorbak25@gmail.com>
2016-01-13 01:30:53 +0100

    [WIP] Visual Debugger
    Signed-off-by: Grzegorz Uriasz <gorbak25@gmail.com>

--- source/dbg/debugger_new.d
+++ source/dbg/debugger_new.d
@@ -3,9 +3,10 @@ module dbg.debug_new;
 import derelict.glfw3.glfw3;
 import glad.gl.all;
 import glad.gl.loader;
-import config;
 
 import std.stdio, std.string, std.file;
+
+import config, scene.camera, scene.scenemgr;
 
 string texture_vert_shader_code = 
 "
@@ -51,7 +52,16 @@ class VisualDebugger
 	float[] saved_rays; //x, y, z
 	float[] point_colors; // r, g, b
 	
+	GLuint vbo_texture;
+	immutable float[] texture_verts = 
+	[-1.0f, -1.0f, 0.0f, 0.0f,
+	1.0f, -1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 0.0f, 1.0f];
 	GLuint traced_texture;
+	
+	WorldSpace space;
+	ICamera camera;
 	
 	this()
 	{
@@ -68,14 +78,53 @@ class VisualDebugger
 				vis_window = makeWindow("GRTrace visualiser", cast(int)cfgResolutionX, cast(int)cfgResolutionY, true);
 				img_window = makeWindow("GRTrace traced image", cast(int)cfgResolutionX, cast(int)cfgResolutionY, false, vis_window);
 			
-				//prepare pipeline programs
+				//prepare "traced image"
+				shaders ~= CompileShader(GL_VERTEX_SHADER, texture_vert_shader_code);
+				shaders ~= CompileShader(GL_FRAGMENT_SHADER, texture_frag_shader_code);
+				
+				GLuint tex_prog = glCreateProgram();
+				glAttachShader(tex_prog, shaders[0]);
+				glAttachShader(tex_prog, shaders[1]);
+				glLinkProgram(tex_prog);
+				programs ~= tex_prog;
+				
+				GLuint tex_vao;
+				glGenVertexArrays(1, &tex_vao);
+				glBindVertexArray(tex_vao);
+				vaos ~= tex_vao;
+				
+				glGenBuffers(1, &vbo_texture);
+				glBindBuffer(GL_ARRAY_BUFFER, vbo_texture);
+				glBufferData(GL_ARRAY_BUFFER, texture_verts.sizeof, cast(void*)texture_verts, GL_STATIC_DRAW);
+				
+				GLint tex_vert_atrib = glGetAttribLocation(tex_prog, "vert");
+				glEnableVertexAttribArray(tex_vert_atrib);
+				glVertexAttribPointer(tex_vert_atrib, 2, GL_FLOAT, GL_FALSE, 4*float.sizeof, cast(void*)0);
+				
+				GLint tex_tex_attrib = glGetAttribLocation(tex_prog, "tex_coord");
+				glEnableVertexAttribArray(tex_tex_attrib);
+				glVertexAttribPointer(tex_vert_atrib, 2, GL_FLOAT, GL_FALSE, 4*float.sizeof, cast(void*)(2*float.sizeof));
+				
 			}
 		}
 	}
 	
 	~this()
 	{
+		glfwDestroyWindow(img_window);
+		glfwDestroyWindow(vis_window);
+		
+		foreach(GLuint i; programs)
+		{
+			glDeleteProgram(i);
+		}
+		foreach(GLuint i; shaders)
+		{
+			glDeleteShader(i);
+		}
+		
 		glfwTerminate();
+		
 	}
 	
 	static VisualDebugger get()
@@ -87,7 +136,7 @@ class VisualDebugger
 		return inst;
 	}
 	
-	GLuint CompileShader(GLenum type, string file_name)
+	GLuint CompileShader(GLuint type, string file_name)
 	{
 		if(exists(file_name))
 		{
@@ -99,7 +148,7 @@ class VisualDebugger
 		}
 	}
 	
-	GLuint CompileShader(GLenum type, char[] code)
+	GLuint CompileShader(GLuint type, char[] code)
 	{
 		//create and compile shader
 		GLuint shader = glCreateShader(type);
@@ -171,6 +220,31 @@ class VisualDebugger
 		glClearColor(0, 0, 0, 1);
 		
 		return w;
+	}
+	
+	void Start()
+	{
+		space = cast(WorldSpace)(cfgSpace);
+		camera = cast(ICamera)(cfgSpace.camera);
+		if(!cfgDebug)return;
+		
+		LoadTexture();
+		
+		
+		while(!(glfwWindowShouldClose(img_window)||glfwWindowShouldClose(vis_window)))
+		{
+			//traced image
+			glfwMakeContextCurrent(img_window);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glUseProgram(programs[0]);
+			glBindVertexArray(vaos[0]);
+			glBindBuffer(GL_ARRAY_BUFFER, vbo_texture);
+			glBindTexture(GL_TEXTURE_2D, traced_texture);
+			glDrawArrays(GL_QUADS, 0, 4);
+			
+			glfwSwapBuffers(img_window);
+			glfwPollEvents();
+		}
 	}
 	
 }
