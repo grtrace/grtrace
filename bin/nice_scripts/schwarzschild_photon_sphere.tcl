de263ddb7fc518cb1ee99acec494f552a998bd71
Grzegorz Uriasz <gorbak25@gmail.com>
2016-03-04 00:59:05 +0100

    Fixed texturing in curved space and more scripts

--- bin/nice_scripts/schwarzschild_photon_sphere.tcl
+++ bin/nice_scripts/schwarzschild_photon_sphere.tcl
@@ -5,17 +5,17 @@ configSet "WorldSpace" "test"
 configSet "SpaceConfig" ""
 configSet "CameraType" "linear"
 configSet "CameraX" 4.5
-configSet "CameraY" 0.0001
-configSet "CameraZ" 0.0001
+configSet "CameraY" 0
+configSet "CameraZ" 0
 configSet "CameraPitch" -6
 configSet "CameraYaw" 0
 configSet "CameraRoll" 0
 configSet "CameraOptions" "-x 10 -f 42"
-configSet "OutputFile" "raytrace_accretion_redshift.png"
-configSet "MetricOptions" "-t schwarzschild -m 1.5 -x 0 -y 0 -z 0 -d 0.05 -n 1000"
+configSet "MetricOptions" "-t schwarzschild -m 1.5 -x 0 -y 0 -z 0 -d 0.01 -n 5000"
 
-addObject .sph1 -m .mat1 -t sphere -c 0,0,0 -r 3
+addMaterial .mat -D -e black -f b
 
+addObject .sph1 -m .mat -t sphere -c 0,0,0 -r 3
 
 makeScene
 puts "Tcl config finished"
