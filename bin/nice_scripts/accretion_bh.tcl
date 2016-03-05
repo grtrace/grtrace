8f45b4efe3322f38c9a84d8d4234cbc0a2ba3fa5
Jakub Szewczyk <kubasz51@gmail.com>
2016-03-05 09:45:10 +0100

    Fixed accretion_bh, optbuild using ldc2

--- bin/nice_scripts/accretion_bh.tcl
+++ bin/nice_scripts/accretion_bh.tcl
@@ -1,5 +1,5 @@
-configSet "ResolutionX" 800
-configSet "ResolutionY" 800
+configSet "ResolutionX" 200
+configSet "ResolutionY" 200
 configSet "Samples" 1
 configSet "WorldSpace" "test"
 configSet "SpaceConfig" ""
@@ -14,9 +14,11 @@ configSet "CameraOptions" "-x 10 -f 42"
 configSet "OutputFile" "raytrace_accretion_redshift.png"
 configSet "MetricOptions" "-t schwarzschild -m 1.5 -x 0 -y 0 -z 0 -d 0.1 -n 500"
 
-addMaterial .mat -D -e black -f b
+addMaterial .mat_black -e 0.0,0.0,0.0 -f n
+addMaterial .mat_disc -l 500 -f n
 
-addObject .sph1 -m .mat1 -t sphere -c 0,0,20 -r 13
+addObject .acr -m .mat_disc -t accretion -c a -o 0,0,0 -a90 -b90 --inner_radius 4.5 --outer_radius 12
+addObject .sph1 -m .mat_black -t sphere -c 0,0,0 -r 3
 
 makeScene
 puts "Tcl config finished"
