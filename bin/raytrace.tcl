680f760e6efaf71dc86b5ccd720a674cce5f5468
Grzegorz Uriasz <gorbak25@gmail.com>
2016-03-26 16:24:19 +0100

    Corrected Christoffel symbols in Kerr Metric
    Unable to obtain photon sphere solution in Kerr Metric

--- bin/raytrace.tcl
+++ bin/raytrace.tcl
@@ -4,14 +4,14 @@ configSet "Samples" 1
 configSet "WorldSpace" "test"
 configSet "SpaceConfig" ""
 configSet "CameraType" "linear"
-configSet "CameraX" 0
-configSet "CameraY" 4
+configSet "CameraX" -4
+configSet "CameraY" 0
 configSet "CameraZ" 0
-configSet "CameraPitch" 0
-configSet "CameraYaw" 270
+configSet "CameraPitch" 90
+configSet "CameraYaw" 0
 configSet "CameraRoll" 0
 configSet "CameraOptions" "-x 10 -f 42"
-configSet "MetricOptions" "-t kerr -m 1 -L 1 -x 0 -y 0 -z 0 -d 0.001 -n 50000"
+configSet "MetricOptions" "-t kerr -m 1 -L 1 -x 0 -y 0 -z 0 -d 0.01 -n 5000"
 
 addMaterial .mat -D -e black -f b
 
