1fcda4adf9e573026c98d7f5deeec065479e215e
Jakub Szewczyk <kubasz51@gmail.com>
2016-03-29 15:05:01 +0200

    Kerr scripts

---
+++ bin/nice_scripts/accretion_kerr.tcl
@@ -0,0 +1,16 @@
+configSet "ResolutionX" 200
+configSet "ResolutionY" 200
+configSet "Samples" 1
+configSet "OutputFile" "raytrace_accretion_kerr.png"
+
+loadScene "scenes/_common.grt"
+loadScene "scenes/bhaccretion_violet.grt"
+sceneCmd "SETCAMERA LINEAR ORIGIN VEC3 0 -8 -80 ANGLES VEC3 -6 0 0 FOV 20 ;"
+# sceneCmd "SETSPACE ANALYTIC SCHWARZSCHILD STEPS 5000 STEPPARAM 0.1 MASS mass ORIGIN vec3.0 ;"
+sceneCmd "SETSPACE ANALYTIC KERR STEPS 5000 STEPPARAM 0.1 MASS mass ORIGIN vec3.0 ANGMOMENTUM 0.5 ;"
+# sceneCmd "SETSPACE FLAT ;"
+
+puts "Tcl config finished"
+doTrace
+waitForTrace
+puts "Tcl script finished"
